7d44e08824315c558dcd0c8ce55e2ad0f8affbf3
Jim Blackler <jimblackler@google.com>
2020-10-06 12:18:53 +0100

    Updates.

--- factors.js
+++ factors.js
@@ -4,51 +4,57 @@ const graph = new Springy.Graph();
 
 const up_to_element = document.getElementById("upto");
 
-const nodes = [];
+let factors = new Map();
 
 function updateDiagram() {
 
   const up_to = up_to_element.value | 0;
-  const number_factors = up_to - 1;
 
   if (up_to != gup('u', 0)) {
     window.history.pushState({}, "", "?u=" + up_to);
   }
 
-  for (let n = 0; n < nodes.length; n++) {
-    graph.detachNode(nodes[n]);
+  for (let node of factors.values()) {
+    graph.detachNode(node);
   }
 
-  for (let n = nodes.length; n < number_factors; n++) {
-    nodes.push(graph.newNode({label: n + 2}));
+  let new_factors = new Map();
+  for (let n = 2; n <= up_to; n++) {
+    if (up_to % n === 0) {
+      if (factors.has(n)) {
+        let node = factors.get(n);
+        new_factors.set(n, node);
+        factors.delete(n);
+      } else {
+        new_factors.set(n, graph.newNode({label: n}));
+      }
+    }
   }
 
-  for (let n = number_factors; n < nodes.length; n++) {
-    graph.removeNode(nodes[n]);
+  for (let node of factors.values()) {
+    graph.removeNode(node);
   }
 
-  nodes.splice(number_factors, nodes.length - number_factors);
+  factors = new_factors;
 
-
-  for (let n = 0; n != number_factors; n++) {
-    const factors = [];
-    const v = n + 2;
+  for (let v of factors.keys()) {
+    const factors2 = [];
 
     for (let f = v / 2 | 0; f > 1; f--) {
       if (v % f !== 0) {
         continue;
       }
       let factor_exists = false;
-      for (let i = 0; i < factors.length; i++) {
-        if (factors[i] % f === 0) {
+      for (let i = 0; i < factors2.length; i++) {
+        if (factors2[i] % f === 0) {
           factor_exists = true;
           break;
         }
       }
 
       if (!factor_exists) {
-        factors.push(f);
-        graph.newEdge(nodes[n], nodes[f - 2], {length: 2});
+        factors2.push(f);
+        graph.newEdge(factors.get(v), factors.get(f), {length: 2});
       }
     }
   }
